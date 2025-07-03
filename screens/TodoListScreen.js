aebbe005c05e19c2c3ca2762891a1f9a19ac8531
JinHyeongSeo <jh4clover@naver.com>
2025-07-03 15:11:36 +0900

    할 일 작성 후, 리스트에 할 일 반영

--- screens/TodoListScreen.js
+++ screens/TodoListScreen.js
@@ -1,14 +1,35 @@
-import {Text,View} from "react-native";
+import {Text,View, StyleSheet} from "react-native";
 import React from "react";
 
-const TodoListScreen = (route) => {
+const TodoListScreen = ({route}) => {
   const { todos } = route.params?.todosState || { todos: [] };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>할일 리스트</Text>
+      {todos.length > 0? (
+        todos.map((todo) => (
+        <View key={todo.id} style={styles.listBox}>
+          <Text>번호: {todo.id}</Text>
+          <Text>작성날짜: {todo.regDate}</Text>
+          <Text>할 일: {todo.content}</Text>
+        </View>
+      ))
+      ) : (
+        <Text style={{fontSize: 20, fontWeight: "bold"}}>할 일이 없습니다.</Text>
+      )}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  listBox: {
+    backgroundColor: "red",
+    width: "90%",
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
+    }
+});
+
 
 export default TodoListScreen;
