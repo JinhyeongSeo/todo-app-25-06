51c238149eb5ad7e0d1db9abce1578959566f034
JinHyeongSeo <jh4clover@naver.com>
2025-07-03 16:36:17 +0900

    삭제 버튼 클릭시 Alert창 나오도록

--- components/TodosProvider.js
+++ components/TodosProvider.js
@@ -4,20 +4,43 @@ import { dateToStr } from "../utils/util";
 const TodosContext = React.createContext();
 
 export const TodosProvider = ({children}) => {
-  const [todos, setTodos] = React.useState([]);
-  const lastTodoIdRef = React.useRef(0);
+    const testTodo = [
+        {
+            id: 1,
+            content: "테니스 치기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 2,
+            content: "리액트 네이티브 공부하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 3,
+            content: "할 일 앱 구현하기",
+            regDate: dateToStr(new Date()),
+        },
+    ]
 
-  const addTodo = (newContent) => {
+    const [todos, setTodos] = React.useState([...testTodo]);
+    const lastTodoIdRef = React.useRef(testTodo.length);
+
+    const addTodo = (newContent) => {
     const id = ++lastTodoIdRef.current;
     const newTodo = {
-      id,
-      content: newContent,
-      regDate: dateToStr(new Date()),
+        id,
+        content: newContent,
+        regDate: dateToStr(new Date()),
     }
 
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
-  };
+    };
+
+    const removeTodo = (id) => {
+
+    }
+
   return (
       <TodosContext.Provider value={{ todos, addTodo }}>
         {children}
