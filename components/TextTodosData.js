f78f65fce23a160759d8c3a00f6e875fcf3e3a15
JinHyeongSeo <jh4clover@naver.com>
2025-07-09 22:37:07 +0900

    할 일 테스트 데이터를 별도의 컴포넌트로 분리

---
+++ components/TextTodosData.js
@@ -0,0 +1,75 @@
+import { dateToStr } from "../utils/util"
+
+const testTodosData = [
+        {
+            id: 1,
+            content: "테니스 치기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 2,
+            content: "리액트 네이티브 공부하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 3,
+            content: "할 일 앱 구현하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 4,
+            content: "운동하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 5,
+            content: "영화 보기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 6,
+            content: "책 읽기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 7,
+            content: "요리하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 8,
+            content: "산책하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 9,
+            content: "음악 감상하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 10,
+            content: "친구 만나기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 11,
+            content: "여행 계획 세우기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 12,
+            content: "새로운 취미 시작하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 13,
+            content: "자기 계발하기",
+            regDate: dateToStr(new Date()),
+        },
+        {
+            id: 14,
+            content: "정리 정돈하기",
+            regDate: dateToStr(new Date()),
+        },
+    ]
+export default testTodosData;
--- components/TodosProvider.js
+++ components/TodosProvider.js
@@ -1,84 +1,14 @@
 import React from "react";
 import { dateToStr } from "../utils/util";
+import testTodosData from "./TextTodosData"; // 테스트용 할 일 데이터
 
 const TodosContext = React.createContext();
 
 export const TodosProvider = ({children}) => {
-    const testTodo = [
-        {
-            id: 1,
-            content: "테니스 치기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 2,
-            content: "리액트 네이티브 공부하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 3,
-            content: "할 일 앱 구현하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 4,
-            content: "운동하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 5,
-            content: "영화 보기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 6,
-            content: "책 읽기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 7,
-            content: "요리하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 8,
-            content: "산책하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 9,
-            content: "음악 감상하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 10,
-            content: "친구 만나기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 11,
-            content: "여행 계획 세우기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 12,
-            content: "새로운 취미 시작하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 13,
-            content: "자기 계발하기",
-            regDate: dateToStr(new Date()),
-        },
-        {
-            id: 14,
-            content: "정리 정돈하기",
-            regDate: dateToStr(new Date()),
-        },
-    ]
+    
 
-    const [todos, setTodos] = React.useState([...testTodo]);
-    const lastTodoIdRef = React.useRef(testTodo.length);
+    const [todos, setTodos] = React.useState([...testTodosData]);
+    const lastTodoIdRef = React.useRef(testTodosData.length);
 
     const addTodo = (newContent) => {
     const id = ++lastTodoIdRef.current;
